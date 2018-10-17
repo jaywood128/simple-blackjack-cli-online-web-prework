036e356645fe51a2422bb808fd151be51942bfb6
Johnathon Wood <jaywood128@gmail.com>
2018-10-17 19:55:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,16 +57,14 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner(sum_total)
+def runner
+  welcome
   card_total = initial_round
   until card_total > 21
-    welcome
     card_total = hit?(card_total)
     display_card_total(card_total)
-  if sum_total > 21
-
    end
    end_game
   end
-end
+
   # code runner here
